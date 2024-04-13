5cb0186dd13a56a43f17e9f82b1098e534820238
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-04-13 15:47:45 +0530

    changed models

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const userModel=require("./users");
+const postModel=require("./posts");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.get('/createUser',(req,res,next)=>{
+
+})
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -14,7 +14,7 @@ const postSchema = new Schema({
   },
   likes: {
     type: Array,
-    default: []
+    default: [ ]
   }
 });
 
